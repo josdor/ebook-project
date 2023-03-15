b48e47225500b6f618c1646bc2ec652c961f8c05
josdor <josdor.r@gmail.com>
2023-03-15 16:50:46 +0100

    ebook-project: init

---
+++ josdor-ebook/josdor-customer-service/src/main/java/com/josdor/customer/JosdorCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.josdor.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosdorCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JosdorCustomerServiceApplication.class, args);
+    }
+
+}
---
+++ josdor-ebook/josdor-customer-service/src/test/java/com/josdor/customer/JosdorCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josdor.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosdorCustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
